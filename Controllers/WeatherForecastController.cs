dcff6928475f2b76d108af9da5837f60f596920c
yonaiker <yonaiker.ocando@unidigital.global>
2022-07-28 14:10:19 -0400

    manejo de rutas

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -3,7 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 namespace APIs.NET.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+//agregamos una ruta para todas las peticiones
+[Route("api/[controller]")]
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
@@ -13,20 +14,52 @@ public class WeatherForecastController : ControllerBase
 
     private readonly ILogger<WeatherForecastController> _logger;
 
+    //creamos una coleccion estatica para almacenar datos en memoria
+    private static List<WeatherForecast> ListWeatherForecast = new List<WeatherForecast>();
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
     {
         _logger = logger;
+
+        //validamos si ListWeatherForecast tiene algun registro
+        if(ListWeatherForecast == null || !ListWeatherForecast.Any())
+        {
+            ListWeatherForecast = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            //generamos una lista
+            .ToList();
+        }
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
+    //agregamos una ruta en espesifico a esta peticion
+    [Route("Get/weatherforecast")]
+    [Route("Get/weatherforecast2")]
+    //creamos una ruta dinamica (agarra el nombre de la peticion (Get))
+    [Route("action")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        return ListWeatherForecast;
+    }
+
+    [HttpPost]
+    public IActionResult Post(WeatherForecast weatherForecast)
+    {   
+        //agregamos a lista lo que pasemos
+        ListWeatherForecast.Add(weatherForecast);
+
+        return Ok();
+    }
+
+    [HttpDelete("{index}")]
+    public IActionResult Delete(int index)
+    {
+        ///removemos de la lista
+        ListWeatherForecast.RemoveAt(index);
+
+        return Ok();
     }
 }
