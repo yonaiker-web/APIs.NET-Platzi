4d9a072a2ca3ab5fd9568be28136d372f0f24a2a
yonaiker <yonaiker.ocando@unidigital.global>
2022-07-28 15:46:59 -0400

    .

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
+using Microsoft.AspNetCore.Builder;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+//en minimal api todos los controladores se generan aqui
 
 app.MapGet("/", () => "Hello World!");
 
