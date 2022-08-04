c917a6154633d177875c9a1f625115d2b84ffe50
yonaiker <yonaiker.ocando@unidigital.global>
2022-08-04 12:08:57 -0400

    add tools de EF y agregando el context y models

---
+++ Models/Category.cs
@@ -0,0 +1,20 @@
+using System.Text.Json.Serialization;
+
+namespace APIs.NET
+{
+    public class Category
+    {
+        //[Key]
+        public Guid CategoryId { get; set; }
+
+        //[Required]
+        //[MaxLength(150)]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Peso { get; set; }
+
+        //creamos una relacion virtual para poder hacer la relacion con la tabla Task
+        [JsonIgnore]
+        public virtual ICollection<Task> Task { get; set; }
+    }
+}
---
+++ Models/Task.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APIs.NET
+{
+    public class Task
+    {
+        //[Key]
+        public Guid TaskId { get; set; }
+
+        //[ForeignKey("CategoryId")]
+        public Guid CategoryId { get; set; }
+
+        //[Required]
+        //[MaxLength(200)]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Priority PriorityTask { get; set; }
+        public DateTime Creation { get; set; }
+        public virtual Category Category { get; set; }
+
+        //[NotMapped]
+        public string Resumen { get; set; }
+    }
+
+    public enum Priority
+    {
+        Baja,
+        Media,
+        Alta
+    }
+}
